16c17cb4583bd08fefa55bd4aa1d06742844ba79
udohgideon <38887517+udohgideon@users.noreply.github.com>
2022-08-11 03:11:56 +0100

    Add files via upload

---
+++ crowdfund.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    document.getElementById("nav-l").classList.toggle("show");
+ };
+
+
+function bookmark(){  
+ let modal = document.querySelector(".modal");
+     modal.style.display = "block";
+};
+
+function closeModal(){
+ let modal = document.querySelector(".modal");
+      modal.style.display = "none";
+}
